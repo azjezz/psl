23a7fcd1603dcfe1a5d2cc9544a36918e6513a2a
azjezz <azjezz@protonmail.com>
2020-07-08 09:37:23 +0100

    fix typo

--- src/Psl/Str/Byte/words.php
+++ src/Psl/Str/Byte/words.php
@@ -7,7 +7,7 @@ namespace Psl\Str\Byte;
 /**
  * Return information about words used in a string.
  *
- * @param string|null $charlist A list of additional characters which will be considered as 'word'
+ * @param string|null $characters_list A list of additional characters which will be considered as 'word'
  *
  * @return array<int, string>   an associative array, where the key is the numeric position of
  *                    the word inside the string and the value is the actual word itself
