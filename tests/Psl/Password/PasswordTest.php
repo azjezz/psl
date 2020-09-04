f96205ea8ea7b57ffca91fe05f3bbdca191f6150
azjezz <azjezz@protonmail.com>
2020-09-05 00:40:26 +0100

    [Password] test argon2i algorithm

--- tests/Psl/Password/PasswordTest.php
+++ tests/Psl/Password/PasswordTest.php
@@ -9,6 +9,7 @@ use Psl\Arr;
 use Psl\Password;
 use Psl\SecureRandom;
 use Psl\Str;
+use const PASSWORD_BCRYPT;
 
 final class PasswordTest extends TestCase
 {
@@ -21,7 +22,7 @@ final class PasswordTest extends TestCase
         self::assertContains(Password\ARGON2I_ALGORITHM, $algorithms);
         self::assertContains(Password\ARGON2ID_ALGORITHM, $algorithms);
 
-        $algos = Arr\map(password_algos(), fn(string $value): string => \PASSWORD_BCRYPT === $value ? Password\BCRYPT_ALGORITHM : $value);
+        $algos = Arr\map(password_algos(), fn(string $value): string => PASSWORD_BCRYPT === $value ? Password\BCRYPT_ALGORITHM : $value);
 
         self::assertSame($algos, $algorithms);
     }
@@ -44,6 +45,24 @@ final class PasswordTest extends TestCase
         self::assertFalse(Password\needs_rehash($hash, Password\BCRYPT_ALGORITHM, [
             'cost' => 8
         ]));
+    }
+
+    /**
+     * @dataProvider providePasswords
+     */
+    public function testArgon2i(string $password): void
+    {
+        $hash = Password\hash($password, Password\ARGON2I_ALGORITHM);
+
+        self::assertTrue(Password\verify($password, $hash));
+
+        $information = Password\get_information($hash);
+        self::assertSame(Password\ARGON2I_ALGORITHM, $information['algorithm']);
+        self::assertSame(Password\ARGON2_DEFAULT_MEMORY_COST, $information['options']['memory_cost']);
+        self::assertSame(Password\ARGON2_DEFAULT_TIME_COST, $information['options']['time_cost']);
+        self::assertSame(Password\ARGON2_DEFAULT_THREADS, $information['options']['threads']);
+
+        self::assertFalse(Password\needs_rehash($hash, Password\ARGON2I_ALGORITHM));
     }
 
     public function providePasswords(): iterable
