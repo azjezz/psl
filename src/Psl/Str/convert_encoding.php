f0662c8085eea5f0f1aba63880f32df86848c5e8
Amit Dudhat <amit000790@gmail.com>
2020-10-02 21:11:11 +0530

    Fix phpdoc and psalm issues.

--- src/Psl/Str/convert_encoding.php
+++ src/Psl/Str/convert_encoding.php
@@ -4,16 +4,22 @@ declare(strict_types=1);
 
 namespace Psl\Str;
 
+use Psl;
+use Psl\Arr;
+
 use function mb_convert_encoding;
 
 /**
  * Convert character encoding of the giving string.
  *
- * @psalm-return string Converts the character encoding of $string to $to_encoding from optionally $from_encoding.
+ * @psalm-return string Converts the character encoding of $string to $to_encoding from $from_encoding.
  *
  * @psalm-pure
  */
 function convert_encoding(string $string, string $from_encoding, string $to_encoding): ?string
 {
+    Psl\invariant(Arr\contains(mb_list_encodings(), $from_encoding), '$from_encoding is invalid.');
+    Psl\invariant(Arr\contains(mb_list_encodings(), $to_encoding), '$to_encoding is invalid.');
+
     return mb_convert_encoding($string, $to_encoding, $from_encoding);
 }
--- src/Psl/Str/is_utf8.php
+++ src/Psl/Str/is_utf8.php
@@ -9,11 +9,11 @@ use function mb_detect_encoding;
 /**
  * Detect the UTF-8 encoding of the giving string.
  *
- * @psalm-return bool The detected UTF-8 character encoding or FALSE if the UTF-8 encoding cannot be detected from the given string.
+ * @psalm-assert-if-true string $string detected UTF-8 character encoding.
  *
  * @psalm-pure
  */
-function is_utf8(string $string): ?bool
+function is_utf8(string $string): bool
 {
     return ! (false === mb_detect_encoding($string, 'UTF-8', true));
 }
