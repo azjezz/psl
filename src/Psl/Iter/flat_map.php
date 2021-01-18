c0ec24ff0bfbaadf9d6f4f9863b52ae3b01d6600
Toon Verwerft <toonverwerft@gmail.com>
2021-01-18 08:47:16 +0100

    [Iter][Arr] flat_map function

---
+++ src/Psl/Iter/flat_map.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\Iter;
+
+/**
+ * @psalm-template Tk of array-key
+ * @psalm-template Tv
+ * @psalm-template T
+ *
+ * @psalm-param iterable<Tk, Tv>            $iterable Iterable to be mapped over
+ * @psalm-param (callable(Tv): iterable<T>) $mapper
+ *
+ * @psalm-return iterable<array-key, T>
+ */
+function flat_map(iterable $iterable, callable $mapper): iterable
+{
+    return Iterator::from(static function () use ($iterable, $mapper) {
+        foreach ($iterable as $value) {
+            foreach ($mapper($value) as $item) {
+                yield $item;
+            }
+        }
+    });
+}
