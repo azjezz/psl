ff71e98c4bc149e5c17dfbafbc4a3d168af7965e
Saif Eddin Gmati <29315886+azjezz@users.noreply.github.com>
2021-02-02 16:33:22 +0100

    [Type] fix parameters name in union function.

--- src/Psl/Type/union.php
+++ src/Psl/Type/union.php
@@ -8,8 +8,8 @@ namespace Psl\Type;
  * @template Tl
  * @template Tr
  *
- * @psalm-param Type<Tl> $left_type_spec
- * @psalm-param Type<Tr> $right_type_spec
+ * @psalm-param Type<Tl> $left_type
+ * @psalm-param Type<Tr> $right_type
  *
  * @psalm-return Type<Tl|Tr>
  */
