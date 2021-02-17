eac00e72ab8a9a6c4983470219e8ce8d1a30826e
azjezz <azjezz@protonmail.com>
2021-02-17 20:46:57 +0100

    [Type] add more tests

---
+++ tests/Psl/Type/IsBoolTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\Tests\Type;
+
+use PHPUnit\Framework\TestCase;
+use Psl\Math;
+use Psl\Type;
+
+final class IsBoolTest extends TestCase
+{
+    public function testIsBool(): void
+    {
+        static::assertTrue(Type\is_bool(true));
+
+        static::assertFalse(Type\is_bool(123));
+        static::assertFalse(Type\is_bool(0));
+        static::assertFalse(Type\is_bool(Math\INT16_MAX));
+        static::assertFalse(Type\is_bool('5'));
+        static::assertFalse(Type\is_bool(null));
+        static::assertFalse(Type\is_bool(5.0));
+    }
+}
---
+++ tests/Psl/Type/IsNullTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\Tests\Type;
+
+use PHPUnit\Framework\TestCase;
+use Psl\Math;
+use Psl\Type;
+
+final class IsNullTest extends TestCase
+{
+    public function testIsNull(): void
+    {
+        static::assertTrue(Type\is_null(null));
+
+        static::assertFalse(Type\is_null(123));
+        static::assertFalse(Type\is_null(0));
+        static::assertFalse(Type\is_null(Math\INT16_MAX));
+        static::assertFalse(Type\is_null('5'));
+        static::assertFalse(Type\is_null(true));
+        static::assertFalse(Type\is_null(5.0));
+    }
+}
