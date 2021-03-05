f32efe45973ad95e0e5b1cda176e213866681b98
azjezz <azjezz@protonmail.com>
2021-03-05 15:30:19 +0100

    [Html] add strip_tags function

---
+++ src/Psl/Html/strip_tags.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\Html;
+
+use function strip_tags as php_strip_tags;
+
+/**
+ * Strip HTML and PHP tags from a string.
+ *
+ * @param list<string> $allowed_tags tags which should not be stripped.
+ *
+ * @psalm-pure
+ */
+function strip_tags(string $html, array $allowed_tags = []): string
+{
+    /**
+     * @psalm-suppress InvalidArgument
+     *
+     * @link https://github.com/vimeo/psalm/issues/5330
+     */
+    return php_strip_tags($html, $allowed_tags);
+}
