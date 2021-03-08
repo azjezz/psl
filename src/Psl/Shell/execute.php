e70b5aed6bcf831247477d0fc6eb4cf6d6e516aa
azjezz <azjezz@protonmail.com>
2021-03-08 20:40:12 +0100

    [Shell] cleanup docblock

--- src/Psl/Shell/execute.php
+++ src/Psl/Shell/execute.php
@@ -19,23 +19,20 @@ use function stream_get_contents;
 /**
  * Execute an external program.
  *
- * @param string                $command            The command to execute.
- * @param list<string>          $arguments          The command arguments listed as separate entries.
- * @param string                $working_directory  The initial working directory for the command.
- *                                                  This must be an absolute directory path, or null
- *                                                  if you want to use the default value (the working dir of the
- *                                                  current directory )
- * @param array<string, string> $environment        An array with the environment variables for the command that
- *                                                  will be run.
- * @param bool                  $escape_arguments   If set to true ( default ), all $arguments will be escaped using
- *                                                  `Shell\escape_argument`.
+ * @param string $command The command to execute.
+ * @param list<string> $arguments The command arguments listed as separate entries.
+ * @param string $working_directory The initial working directory for the command.
+ *  This must be an absolute directory path, or null if you want to use the default value (the working dir of the
+ *  current directory )
+ * @param array<string, string> $environment A dict with the environment variables for the command that
+ *  will be run.
+ * @param bool $escape_arguments If set to true ( default ), all $arguments will be escaped using `escape_argument`.
  *
  * @psalm-taint-sink shell $command
  *
- * @throws Exception\FailedExecutionException   In case the command resulted in an exit code other than 0.
- * @throws Exception\PossibleAttackException    In case the command being run is suspicious ( e.g: contains NULL byte ).
- * @throws Exception\RuntimeException           In case $working_directory doesn't exist, or unable to create a new
- *                                              process.
+ * @throws Exception\FailedExecutionException In case the command resulted in an exit code other than 0.
+ * @throws Exception\PossibleAttackException In case the command being run is suspicious ( e.g: contains NULL byte ).
+ * @throws Exception\RuntimeException In case $working_directory doesn't exist, or unable to create a new process.
  */
 function execute(
     string $command,
