c8524d2d8d0981ee194c6ee4e3adf6dda30977b7
azjezz <azjezz@protonmail.com>
2021-03-26 08:52:40 +0100

    add `Dict\count_values` test

---
+++ tests/Psl/Dict/CountValuesTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\Tests\Dict;
+
+use PHPUnit\Framework\TestCase;
+use Psl\Collection;
+use Psl\Dict;
+use Psl\Vec;
+
+final class CountValuesTest extends TestCase
+{
+    /**
+     * @dataProvider provideData
+     */
+    public function testCountValues(array $expected, array $array): void
+    {
+        static::assertSame($expected, Dict\count_values($array));
+    }
+
+    public function provideData(): array
+    {
+        return [
+            [
+                ['foo' => 1],
+                ['foo'],
+            ],
+            [
+                ['foo' => 2, 'bar' => 1, 'baz' => 5],
+                ['foo', 'bar', 'baz', 'foo', 'baz', 'baz', 'baz', 'baz'],
+            ],
+            [
+                ['foo' => 2, 'bar' => 1, 'baz' => 5],
+                Collection\Vector::fromArray(['foo', 'bar', 'baz', 'foo', 'baz', 'baz', 'baz', 'baz']),
+            ],
+            [
+                ['foo' => 2, 'bar' => 1, 'baz' => 4],
+                Vec\concat(
+                    ['foo', 'bar', 'baz'],
+                    ['foo'],
+                    ['baz'],
+                    ['baz', 'baz'],
+                ),
+            ],
+        ];
+    }
+}
