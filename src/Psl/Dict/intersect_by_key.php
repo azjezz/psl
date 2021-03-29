d3149d5326d525fc62305c89833652010c957da0
Saif Eddin Gmati <29315886+azjezz@users.noreply.github.com>
2021-03-29 07:42:48 +0100

    [Dict] remove generic closures usage

--- src/Psl/Dict/intersect_by_key.php
+++ src/Psl/Dict/intersect_by_key.php
@@ -30,9 +30,6 @@ function intersect_by_key(iterable $first, iterable $second, iterable ...$rest):
     return array_intersect_key(from_iterable($first), from_iterable($second), ...Vec\map(
         $rest,
         /**
-         * @template Tk of array-key
-         * @template Tv
-         *
          * @param iterable<Tk, Tv> $iterable
          *
          * @return array<Tk, Tv>
