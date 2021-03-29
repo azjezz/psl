67681066e619b3ec4da61604fd2b5a504327368b
Saif Eddin Gmati <29315886+azjezz@users.noreply.github.com>
2021-03-29 07:43:18 +0100

    [Dict] remove generic closures usage

--- src/Psl/Dict/diff_by_key.php
+++ src/Psl/Dict/diff_by_key.php
@@ -30,9 +30,6 @@ function diff_by_key(iterable $first, iterable $second, iterable ...$rest): arra
     return array_diff_key(from_iterable($first), from_iterable($second), ...Vec\map(
         $rest,
         /**
-         * @template Tk of array-key
-         * @template Tv
-         *
          * @param iterable<Tk, Tv> $iterable
          *
          * @return array<Tk, Tv>
