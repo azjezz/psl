468f91f954ce095aed37c98357bdf3274cbdd8ff
Claas Augner <github@caugner.de>
2021-07-09 17:25:38 +0200

    [Regex] Change replace_with $callback to accept string[]
    The $matches array that is passed to `preg_replace_callback()`'s $callback 
    can have string keys when the pattern uses named capture groups.
    
    See: https://github.com/vimeo/psalm/pull/5608

--- src/Psl/Regex/replace_with.php
+++ src/Psl/Regex/replace_with.php
@@ -15,7 +15,7 @@ use function preg_replace_callback;
  * `$callback`.
  *
  * @param non-empty-string $pattern The pattern to search for.
- * @param (callable(array<int, string>): string) $callback The replacement callable.
+ * @param (callable(string[]): string) $callback The replacement callable.
  * @param null|positive-int $limit The maximum possible replacements for
  *                                 $pattern within $haystack.
  *
