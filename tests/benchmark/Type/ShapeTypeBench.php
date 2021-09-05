959c3e9661c24459e7603dd960f7d7453e8a8bd0
Marco Pivetta <ocramius@gmail.com>
2021-09-05 15:35:10 +0200

    Added iterable types shape coercion benchmarks

--- tests/benchmark/Type/ShapeTypeBench.php
+++ tests/benchmark/Type/ShapeTypeBench.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Psl\Tests\Benchmark\Type;
 
+use ArrayIterator;
 use PhpBench\Attributes\BeforeMethods;
 
 use Psl\Type\Exception\CoercionException;
@@ -15,6 +16,23 @@ use function Psl\Type\shape;
 #[BeforeMethods('__construct')]
 final class ShapeTypeBench
 {
+    private const NON_EMPTY_ARRAY = ['foo' => 'bar'];
+    private const VALID_STRUCTURE = [
+        'foo' => null,
+        'bar' => null,
+        'baz' => null,
+    ];
+    private const VALID_STRUCTURE_WITH_FURTHER_VALUES = [
+        'foo' => null,
+        'bar' => null,
+        'baz' => null,
+        'tab' => null,
+        'taz' => null,
+        'tar' => null,
+        'waz' => null,
+        'war' => null,
+    ];
+
     /**
      * @var TypeInterface<array>
      */
@@ -23,6 +41,10 @@ final class ShapeTypeBench
      * @var TypeInterface<array>
      */
     private TypeInterface $complexShapeWithOptionalValues;
+    private iterable $emptyIterable;
+    private iterable $nonEmptyIterable;
+    private iterable $validStructureIterable;
+    private iterable $validStructureWithFurtherValuesIterable;
 
     public function __construct()
     {
@@ -33,6 +55,10 @@ final class ShapeTypeBench
             'baz' => mixed(),
             'tab' => optional(mixed()),
         ], true);
+        $this->emptyIterable = new ArrayIterator([]);
+        $this->nonEmptyIterable = new ArrayIterator(self::NON_EMPTY_ARRAY);
+        $this->validStructureIterable = new ArrayIterator(self::VALID_STRUCTURE);
+        $this->validStructureWithFurtherValuesIterable = new ArrayIterator(self::VALID_STRUCTURE_WITH_FURTHER_VALUES);
     }
 
     /**
@@ -46,9 +72,25 @@ final class ShapeTypeBench
     /**
      * @throws CoercionException
      */
+    public function benchEmptyShapeCoercionAgainstEmptyIterable(): array
+    {
+        return $this->emptyShape->coerce($this->emptyIterable);
+    }
+
+    /**
+     * @throws CoercionException
+     */
     public function benchEmptyShapeCoercionAgainstNonEmptyArray(): array
     {
-        return $this->emptyShape->coerce(['foo' => 'bar']);
+        return $this->emptyShape->coerce(self::NON_EMPTY_ARRAY);
+    }
+
+    /**
+     * @throws CoercionException
+     */
+    public function benchEmptyShapeCoercionAgainstNonEmptyIterable(): array
+    {
+        return $this->emptyShape->coerce($this->nonEmptyIterable);
     }
 
     /**
@@ -56,11 +98,15 @@ final class ShapeTypeBench
      */
     public function benchComplexShapeCoercionAgainstValidStructure(): array
     {
-        return $this->complexShapeWithOptionalValues->coerce([
-            'foo' => null,
-            'bar' => null,
-            'baz' => null,
-        ]);
+        return $this->complexShapeWithOptionalValues->coerce(self::VALID_STRUCTURE);
+    }
+
+    /**
+     * @throws CoercionException
+     */
+    public function benchComplexShapeCoercionAgainstValidStructureIterable(): array
+    {
+        return $this->complexShapeWithOptionalValues->coerce($this->validStructureIterable);
     }
 
     /**
@@ -68,15 +114,14 @@ final class ShapeTypeBench
      */
     public function benchComplexShapeCoercionAgainstValidStructureWithFurtherValues(): array
     {
-        return $this->complexShapeWithOptionalValues->coerce([
-            'foo' => null,
-            'bar' => null,
-            'baz' => null,
-            'tab' => null,
-            'taz' => null,
-            'tar' => null,
-            'waz' => null,
-            'war' => null,
-        ]);
+        return $this->complexShapeWithOptionalValues->coerce(self::VALID_STRUCTURE_WITH_FURTHER_VALUES);
+    }
+
+    /**
+     * @throws CoercionException
+     */
+    public function benchComplexShapeCoercionAgainstValidStructureWithFurtherValuesIterable(): array
+    {
+        return $this->complexShapeWithOptionalValues->coerce($this->validStructureWithFurtherValuesIterable);
     }
 }
