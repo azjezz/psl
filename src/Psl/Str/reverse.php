a48b0805afb3c99e1a7fda4d9cd14038921bc0eb
Ivan Yivoff <ivan@yivoff.com>
2021-10-17 16:49:36 +0200

    Psl\Str\reverse(string):string function, to mirror Psl\Str\Byte\reverse

---
+++ src/Psl/Str/reverse.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\Str;
+
+use Psl\Vec;
+
+/**
+ * Returns the given string reversed.
+ *
+ */
+function reverse(string $string, ?string $encoding = null): string
+{
+        $parts = split($string, '', null, $encoding);
+        return join(Vec\reverse($parts), '');
+}
