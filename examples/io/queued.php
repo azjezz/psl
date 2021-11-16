6dc1dc032e5590203f1f559df0c62259c6866f92
azjezz <azjezz@protonmail.com>
2021-11-16 13:22:49 +0100

    feat(io): queue operations instead of throwing
    Signed-off-by: azjezz <azjezz@protonmail.com>

---
+++ examples/io/queued.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\Example\IO;
+
+use Psl;
+use Psl\Async;
+use Psl\IO;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+Async\main(static function(): int {
+    [$read, $write] = IO\pipe();
+
+    $he = Async\run(static fn(): string => $read->readFixedSize(2));
+
+    Async\sleep(0.001);
+
+    $write->write("hello");
+
+    $llo = $read->readFixedSize(3);
+
+    Psl\invariant($he->isComplete(), 'First read should have completed before second one.');
+    Psl\invariant('llo' === $llo, 'First read should have completed before second one.');
+
+    IO\output_handle()->write($he->await() . $llo);
+
+    return 0;
+});
