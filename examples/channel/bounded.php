42807a71a72252b6b966feb9dad22bd6d414e413
azjezz <azjezz@protonmail.com>
2021-12-02 15:04:41 +0100

    chore(channel): improve performance
    Signed-off-by: azjezz <azjezz@protonmail.com>

--- examples/channel/bounded.php
+++ examples/channel/bounded.php
@@ -11,32 +11,36 @@ use Psl\IO;
 
 require __DIR__ . '/../../vendor/autoload.php';
 
-/**
- * @var Channel\ReceiverInterface<string> $receiver
- * @var Channel\SenderInterface<string> $sender
- */
-[$receiver, $sender] = Channel\bounded(10);
+Async\main(static function () {
+    /**
+     * @var Channel\ReceiverInterface<string> $receiver
+     * @var Channel\SenderInterface<string> $sender
+     */
+    [$receiver, $sender] = Channel\bounded(10);
 
-Async\Scheduler::defer(static function () use ($receiver) {
-    try {
-        while (true) {
-            $receiver->receive();
-
-            IO\output_handle()->writeAll('.');
+    Async\Scheduler::defer(static function () use ($receiver) {
+        try {
+            while (true) {
+                $receiver->receive();
+            }
+        } catch (Channel\Exception\ClosedChannelException) {
+            IO\output_handle()->write("[ receiver ]: completed.\n");
         }
-    } catch (Channel\Exception\ClosedChannelException) {
-        return;
+    });
+
+    for ($i = 0; $i < 10; $i++) {
+        $file = File\open_read_only(__FILE__);
+        $reader = new IO\Reader($file);
+        while (!$reader->isEndOfFile()) {
+            $byte = $reader->readByte();
+
+            $sender->send($byte);
+        }
     }
+
+    IO\output_handle()->writeAll("[ sender   ]: completed.\n");
+    $sender->close();
+
+    return 0;
 });
 
-for ($i = 0; $i < 10; $i++) {
-    $file = File\open_read_only(__FILE__);
-    $reader = new IO\Reader($file);
-    while (!$reader->isEndOfFile()) {
-        $byte = $reader->readByte();
-
-        $sender->send($byte);
-    }
-}
-
-IO\output_handle()->writeAll("\ndone\n");
