de21dd13910538a9465ea66f063ba9370a44f015
azjezz <azjezz@protonmail.com>
2021-12-08 22:12:54 +0100

    chore(async): add main example
    Signed-off-by: azjezz <azjezz@protonmail.com>

---
+++ examples/async/main.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\Example\IO;
+
+use Psl\Async;
+use Psl\Shell;
+use Psl\IO;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+Async\main(static function(): int {
+  $watcher = Async\Scheduler::onSignal(SIGINT, function (): never {
+      IO\write_line('SIGINT received, stopping...');
+      exit(0);
+  });
+
+  Async\Scheduler::unreference($watcher);
+
+  IO\write_error_line('Press Ctrl+C to stop');
+
+  Async\parallel([
+    static fn(): string => Shell\execute('sleep', ['3']),
+    static fn(): string => Shell\execute('echo', ['Hello World!']),
+    static fn(): string => Shell\execute('echo', ['Hello World!']),
+  ]);
+
+  IO\write_error_line('Done!');
+
+  return 0;
+});
