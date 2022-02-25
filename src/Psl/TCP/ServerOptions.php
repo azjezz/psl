47d4e5e897b08e90bb412065fd43abe0680b86b3
azjezz <azjezz@protonmail.com>
2022-02-25 17:30:02 +0100

    feat(network): support idle connections
    Signed-off-by: azjezz <azjezz@protonmail.com>

--- src/Psl/TCP/ServerOptions.php
+++ src/Psl/TCP/ServerOptions.php
@@ -8,27 +8,43 @@ use Psl\Network;
 
 final class ServerOptions
 {
+    /**
+     * @param int<1, max> $idleConnections
+     */
     public function __construct(
         public readonly bool $noDelay,
+        public readonly int $idleConnections,
         public readonly Network\SocketOptions $socketOptions,
     ) {
     }
 
+    /**
+     * @param int<1, max> $idleConnections
+     */
     public static function create(
         bool $noDelay = false,
+        int $idleConnections = 1000,
         ?Network\SocketOptions $socketOptions = null,
     ): ServerOptions {
-        return new self($noDelay, $socketOptions ?? Network\SocketOptions::create());
+        return new self($noDelay, $idleConnections, $socketOptions ?? Network\SocketOptions::create());
     }
 
     public function withSocketOptions(
         Network\SocketOptions $socketOptions
     ): ServerOptions {
-        return new self($this->noDelay, $socketOptions);
+        return new self($this->noDelay, $this->idleConnections, $socketOptions);
     }
 
     public function withNoDelay(bool $enabled = true): ServerOptions
     {
-        return new self($enabled, $this->socketOptions);
+        return new self($enabled, $this->idleConnections, $this->socketOptions);
+    }
+
+    /**
+     * @param int<1, max> $idleConnections
+     */
+    public function withIdleConnections(int $idleConnections): ServerOptions
+    {
+        return new self($this->noDelay, $idleConnections, $this->socketOptions);
     }
 }
