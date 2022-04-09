99dfd1135a2a92a8b3913bca09eaaf0b30c7a93c
Jeremiasz Major <jrh.mjr@gmail.com>
2022-04-09 20:41:31 +0200

    chore(dict): add test for `slice`

---
+++ tests/unit/Dict/SliceTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\Tests\Unit\Dict;
+
+use PHPUnit\Framework\TestCase;
+use Psl\Dict;
+
+final class SliceTest extends TestCase
+{
+    /**
+     * @dataProvider provideData
+     */
+    public function testSlice(array $expected, array $array, int $n, int $l = null): void
+    {
+        $result = Dict\slice($array, $n, $l);
+
+        static::assertSame($expected, $result);
+    }
+
+    public function provideData(): iterable
+    {
+        yield [['c' => 3, 'd' => 4], ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4], 2];
+        yield [['b' => 2, 'c' => 3], ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4], 1, 2];
+    }
+}
