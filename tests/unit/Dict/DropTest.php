0bfbc2cf64267e8b4daa17ffcb547926e1e73be2
Jeremiasz Major <jrh.mjr@gmail.com>
2022-04-09 20:43:45 +0200

    chore(dict): add more tests

--- tests/unit/Dict/DropTest.php
+++ tests/unit/Dict/DropTest.php
@@ -22,6 +22,7 @@ final class DropTest extends TestCase
     public function provideData(): iterable
     {
         yield [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], 0];
+        yield [['c' => 3, 'd' => 4], ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4], 2];
         yield [[3 => 4, 4 => 5], [1, 2, 3, 4, 5], 3];
         yield [[2 => 3, 3 => 4, 4 => 5], [1, 2, 3, 4, 5], 2];
         yield [[], [1, 2, 3, 4, 5], 5];
