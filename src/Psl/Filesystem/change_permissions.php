cc478ceeec705e2ffe493c874df0a4fd4943ac7a
Jeremiasz Major <jrh.mjr@gmail.com>
2022-04-11 17:00:53 +0200

    chore(filesystem): regenerate docs

--- src/Psl/Filesystem/change_permissions.php
+++ src/Psl/Filesystem/change_permissions.php
@@ -13,7 +13,7 @@ use function chmod;
  * Change permission mode of $node.
  *
  * @param non-empty-string $node
- * @param int<0, 511> $permissions Permissions as an octal number, e.g. `0755`.
+ * @param int $permissions Permissions as an octal number, e.g. `0755`.
  *
  * @throws Exception\RuntimeException If unable to change the mode for the given $node.
  * @throws Exception\NotFoundException If $node does not exist.
