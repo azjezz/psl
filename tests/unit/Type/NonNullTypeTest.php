fedb2e312b9ce394049203c9ba91be8c56d02ec3
Saif Eddin Gmati <29315886+azjezz@users.noreply.github.com>
2023-01-15 15:59:48 +0100

    feat(type): add nonnull type (#392)

---
+++ tests/unit/Type/NonNullTypeTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\Tests\Unit\Type;
+
+use Psl\Type;
+
+final class NonNullTypeTest extends TypeTest
+{
+    public function getType(): Type\TypeInterface
+    {
+        return Type\nonnull();
+    }
+
+    public function getValidCoercions(): iterable
+    {
+        yield [$_ = Type\bool(), $_];
+        yield [$_ = 1, $_];
+        yield [$_ = 0, $_];
+        yield [$_ = false, $_];
+        yield [$_ = true, $_];
+        yield [$_ = '', $_];
+        yield [$_ = 'null', $_];
+        yield [$_ = 'foo', $_];
+        yield [$_ = [null], $_];
+        yield [$_ = [], $_];
+        yield [$_ = [1, 2, 3], $_];
+        yield [$_ = $this->stringable(''), $_];
+    }
+
+    public function getInvalidCoercions(): iterable
+    {
+        yield [null];
+    }
+
+    public function getToStringExamples(): iterable
+    {
+        yield [$this->getType(), 'nonnull'];
+    }
+}
