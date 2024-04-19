fc93c4894cf496e0a5b99f04cc607775062a0cf9
Simon Podlipsky <simon@podlipsky.net>
2024-04-19 10:11:41 +0200

    Revert "feat: narrow ResultInterface to concrete implementation after calling `isSucceeded()` and `isFailed()` (#466)"
    This reverts commit d2114541a5402cb7fcff99548b246ff187e9ac68.

--- src/Psl/Result/ResultInterface.php
+++ src/Psl/Result/ResultInterface.php
@@ -115,8 +115,6 @@ interface ResultInterface extends Psl\Promise\PromiseInterface
      * @return bool - `true` if the operation succeeded; `false` otherwise
      *
      * @psalm-mutation-free
-     *
-     * @psalm-assert-if-true Success<T> $this
      */
     public function isSucceeded(): bool;
 
@@ -128,8 +126,6 @@ interface ResultInterface extends Psl\Promise\PromiseInterface
      * @return bool - `true` if the operation failed; `false` otherwise
      *
      * @psalm-mutation-free
-     *
-     * @psalm-assert-if-true Failure<T> $this
      */
     public function isFailed(): bool;
 
