79a861e07da7180e86fcdcb2f0d894adedfdbe0d
jb-phpjob <6864389+phpjob@users.noreply.github.com>
2024-09-13 12:26:58 +0200

    fix: change duration to load the correct class in loader (#492)

--- src/Psl/Internal/Loader.php
+++ src/Psl/Internal/Loader.php
@@ -871,7 +871,7 @@ final class Loader
         'Psl\\DateTime\\Exception\\ParserException' => 'Psl/DateTime/Exception/ParserException.php',
         'Psl\\DateTime\\Exception\\UnderflowException' => 'Psl/DateTime/Exception/UnderflowException.php',
         'Psl\\DateTime\\DateTime' => 'Psl/DateTime/DateTime.php',
-        'Psl\\DateTime\\Duration' => 'Psl/DateTime/Interval.php',
+        'Psl\\DateTime\\Duration' => 'Psl/DateTime/Duration.php',
         'Psl\\DateTime\\Timestamp' => 'Psl/DateTime/Timestamp.php',
     ];
 
