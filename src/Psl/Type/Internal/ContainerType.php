01013ba44060439982780bfc75e153ba38166e4d
azjezz <azjezz@protonmail.com>
2025-03-03 01:06:14 +0100

    feat(type): add container type
    Signed-off-by: azjezz <azjezz@protonmail.com>

---
+++ src/Psl/Type/Internal/ContainerType.php
@@ -0,0 +1,149 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\Type\Internal;
+
+use Psl\Iter;
+use Psl\Str;
+use Psl\Type;
+use Psl\Type\Exception\AssertException;
+use Psl\Type\Exception\CoercionException;
+use Throwable;
+
+use function is_iterable;
+
+/**
+ * @template Tk as array-key
+ * @template Tv
+ *
+ * @extends Type\Type<iterable<Tk, Tv>>
+ *
+ * @internal
+ */
+final readonly class ContainerType extends Type\Type
+{
+    /**
+     * @psalm-mutation-free
+     *
+     * @param Type\TypeInterface<Tk> $key_type
+     * @param Type\TypeInterface<Tv> $value_type
+     */
+    public function __construct(
+        private readonly Type\TypeInterface $key_type,
+        private readonly Type\TypeInterface $value_type,
+    ) {
+    }
+
+    /**
+     * @throws CoercionException
+     *
+     * @return iterable<Tk, Tv>
+     */
+    #[\Override]
+    public function coerce(mixed $value): iterable
+    {
+        if (!is_iterable($value)) {
+            throw CoercionException::withValue($value, $this->toString());
+        }
+
+        /** @var Type\Type<Tk> $key_type */
+        $key_type = $this->key_type;
+        /** @var Type\Type<Tv> $value_type_speec */
+        $value_type = $this->value_type;
+
+        /** @var array<Tk, Tv> $values */
+        $values = [];
+
+        $k = null;
+        $v = null;
+        $trying_key = true;
+        $iterating = true;
+
+        try {
+            /**
+             * @var Tk $k
+             * @var Tv $v
+             */
+            foreach ($value as $k => $v) {
+                $iterating = false;
+                $trying_key = true;
+                $k_result = $key_type->coerce($k);
+                $trying_key = false;
+                $v_result = $value_type->coerce($v);
+
+                $values[$k_result] = $v_result;
+                $iterating = true;
+            }
+        } catch (Throwable $e) {
+            throw match (true) {
+                $iterating => CoercionException::withValue(
+                    null,
+                    $this->toString(),
+                    PathExpression::iteratorError($k),
+                    $e,
+                ),
+                $trying_key => CoercionException::withValue($k, $this->toString(), PathExpression::iteratorKey($k), $e),
+                !$trying_key => CoercionException::withValue($v, $this->toString(), PathExpression::path($k), $e),
+            };
+        }
+
+        /** @var iterable<Tk, Tv> */
+        return $values;
+    }
+
+    /**
+     * @throws AssertException
+     *
+     * @return iterable<Tk, Tv>
+     *
+     * @psalm-assert iterable<Tk, Tv> $value
+     */
+    #[\Override]
+    public function assert(mixed $value): iterable
+    {
+        if (!is_iterable($value)) {
+            throw AssertException::withValue($value, $this->toString());
+        }
+        /** @var Type\Type<Tk> $key_type */
+        $key_type = $this->key_type;
+        /** @var Type\Type<Tv> $value_type */
+        $value_type = $this->value_type;
+
+        /** @var array<Tk, Tv> $$values */
+        $values = [];
+
+        $k = null;
+        $v = null;
+        $trying_key = true;
+
+        try {
+            /**
+             * @var Tk $k
+             * @var Tv $v
+             */
+            foreach ($value as $k => $v) {
+                $trying_key = true;
+                $k_result = $key_type->assert($k);
+                $trying_key = false;
+                $v_result = $value_type->assert($v);
+
+                $values[$k_result] = $v_result;
+            }
+        } catch (AssertException $e) {
+            throw match ($trying_key) {
+                true => AssertException::withValue($k, $this->toString(), PathExpression::iteratorKey($k), $e),
+                false => AssertException::withValue($v, $this->toString(), PathExpression::path($k), $e),
+            };
+        }
+
+        /** @var iterable<Tk, Tv> */
+        return $values;
+    }
+
+    #[\Override]
+    public function toString(): string
+    {
+        return Str\format('container<%s, %s>', $this->key_type->toString(), $this->value_type->toString());
+    }
+}
---
+++ src/Psl/Type/container.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\Type;
+
+/**
+ * @psalm-pure
+ *
+ * @template Tk as array-key
+ * @template Tv
+ *
+ * @param TypeInterface<Tk> $key_type
+ * @param TypeInterface<Tv> $value_type
+ *
+ * @return TypeInterface<iterable<Tk, Tv>>
+ */
+function container(TypeInterface $key_type, TypeInterface $value_type): TypeInterface
+{
+    return new Internal\ContainerType($key_type, $value_type);
+}
