fb444d0849e74acf9f8af084a01f9b652b1093a0
Saif Eddin Gmati <29315886+azjezz@users.noreply.github.com>
2025-09-13 06:41:26 +0100

    refactor: improve type inference for `Iter` and `Regex` (#528)
    This commit refines the PHPDoc return types for several functions in the `Psl\Iter` and `Psl\Regex` namespaces to provide more accurate information to static analysis tools like Mago and Psalm.
    
    By leveraging conditional types, Mago can now correctly infer non-nullable return types when operating on non-empty arrays and more precise match structures in regex functions.
    This improves developer experience by reducing false positives and eliminating the need for redundant user-land assertions.
    
    Signed-off-by: azjezz <azjezz@protonmail.com>

--- src/Psl/Iter/first.php
+++ src/Psl/Iter/first.php
@@ -11,7 +11,7 @@ namespace Psl\Iter;
  *
  * @param iterable<T> $iterable
  *
- * @return T|null
+ * @return ($iterable is non-empty-array ? T : null)
  *
  * @mago-expect lint:loop-does-not-iterate
  */
