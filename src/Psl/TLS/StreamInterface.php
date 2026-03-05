18af1635492fe6f7e22b54b74564e18c4f5e5836
Seifeddine Gmati <29315886+azjezz@users.noreply.github.com>
2026-03-05 17:03:07 +0100

    feat(tls): introduce TCPConnector for poolable TLS connections (#612)

--- src/Psl/TLS/StreamInterface.php
+++ src/Psl/TLS/StreamInterface.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Psl\TLS;
 
-use Psl\Network;
+use Psl\TCP;
 
 /**
  * A TLS-encrypted network stream with access to TLS connection state.
  */
-interface StreamInterface extends Network\StreamInterface
+interface StreamInterface extends TCP\StreamInterface
 {
     /**
      * Returns the TLS connection state for this connection.
